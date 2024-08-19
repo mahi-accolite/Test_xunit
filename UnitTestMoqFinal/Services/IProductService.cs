842b63a6ce8b48f943f96b3dc37571a507002ece
mahendra-accolitedigital <101805723+mahi-accolite@users.noreply.github.com>
2024-08-19 21:22:31 +0530

    Add project files.

---
+++ UnitTestMoqFinal/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using UnitTestMoqFinal.Models;
+
+namespace UnitTestMoqFinal.Services
+{
+    public interface IProductService
+    {
+        public IEnumerable<Product> GetProductList();
+        public Product GetProductById(int id);
+        public Product AddProduct(Product product);
+        public Product UpdateProduct(Product product);
+        public bool DeleteProduct(int Id);
+    }
+}
